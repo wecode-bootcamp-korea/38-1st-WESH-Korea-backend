f1d0db75a33e45ab0f296bbdc34f3c5f6fe7b384
김성훈 <m1_air@M1air.local>
2022-10-18 08:49:48 +0900

    .env.sample파일 수정 및 app.js에 db연동 에러핸들링 추가

---
+++ app.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const http = require('http');
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const routes = require('./api/routes') ;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.get('/ping', async (req, res) => {
+    res.status(200).json({message: '!! 연결 완료 !!'})
+})
+
+const server = http.createServer(app);
+const PORT = process.env.PORT;
+
+const start = async () => {
+
+    await appDataSource.initialize()
+    .then(() => {
+      console.log("Data Source has been initialized");
+    })
+    .catch(() => {
+      console.log("Errors occurred in Data Source initializing");
+      appDataSource.destroy();
+    })
+
+  try {
+    app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+start();
