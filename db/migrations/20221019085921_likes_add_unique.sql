d62ed459d406760403a08e25b3e7009cc2bbb599
정재욱 <dnrdls77@gmail.com>
2022-10-20 10:55:08 +0900

    likes table unique constraint 추가

--- db/migrations/20221019085921_likes_add_unique.sql
+++ db/migrations/20221019085921_likes_add_unique.sql
@@ -1,5 +1,5 @@
 -- migrate:up
-ALTER TABLE likes ADD UNIQUE (user_id, product_id);
+ALTER TABLE likes ADD CONSTRAINT unique_likes UNIQUE (user_id, product_id);
 
 -- migrate:down
-ALTER TABLE likes DROP UNIQUE (user_id, product_id);
+ALTER TABLE likes DROP CONSTRAINT unique_likes;
