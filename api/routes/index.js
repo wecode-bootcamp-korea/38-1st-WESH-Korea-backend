16b26546367009efc8aefdc4491424538ef1a2e9
정재욱 <dnrdls77@gmail.com>
2022-10-21 14:07:00 +0900

    routes 컨벤션 통일

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userRouter = require('./userRouter');
-const cartRouter = require("./cartRoute");
+const cartRouter = require('./cartRoute');
 
 router.use('/user', userRouter);
-router.use("/cart", cartRouter.router);
+router.use('/cart', cartRouter.router);
 
 module.exports = router;
