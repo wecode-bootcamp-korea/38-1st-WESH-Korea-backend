d2f319654f07ea902fce765555ddd1ee619b9f76
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-10-24 15:45:14 +0900

    [productlist]

--- api/models/productDao.js
+++ api/models/productDao.js
@@ -21,41 +21,72 @@ const getSELLCount = async () =>{
     `)
 }
 const getProductList= async(cateId, limit, offset, order)=>{
-    const input = [order, limit, offset];
-    let queryByCategory;
-    if(!cateId){
-        queryByCategory =
-        `SELECT
-        p.id,
-        p.name AS title,
-        p.price,
-        thumbnail AS img,
-        sc.name AS category
-    FROM products p
-    JOIN sub_categories sc ON p.sub_category_id = sc.id
-    ?
-    LIMIT ? OFFSET ?;`;
-    } else {
-        input.unshift(cateId);
-        queryByCategory =
-        `SELECT
-            p.id,
-            p.name AS title,
-            p.price,
-            thumbnail AS img,
-            sc.name AS category,
-            COUNT(r.content)
-        FROM products p
-        JOIN sub_categories sc ON p.sub_category_id = sc.id
-        LEFT JOIN reviews r ON r.product_id = p.id
-        LEFT JOIN user_product up ON up.product_id = p.id
-        WHERE sub_category_id = ?
-        GROUP BY p.id
-        ORDER BY ?
-        LIMIT ? OFFSET ?;`;
-    }
-    return appDataSource.query(queryByCategory, input);
+const input = [ limit, offset];
+let query = !cateId ? `
+    SELECT 
+        p.id, 
+        p.name AS title, 
+        p.price, 
+        thumbnail AS img, 
+        sc.name AS category 
+    FROM products p 
+    JOIN sub_categories sc ON p.sub_category_id = sc.id 
+    ORDER BY ${order}
+    LIMIT ? OFFSET ? ;
+    ` : `
+    SELECT 
+        p.id, 
+        p.name AS title, 
+        p.price, 
+        thumbnail AS img, 
+        sc.name AS category 
+    FROM products p 
+    JOIN sub_categories sc ON p.sub_category_id = sc.id 
+    WHERE p.sub_category_id = 3
+    ORDER BY ${order}
+    LIMIT 16 OFFSET 0 ;` 
+    if(cateId) input.unshift(cateId);
+    let sol = await appDataSource.query(query, input);
+    return sol;
 }
+//sql injection
+const getProductListByReview= async(cateId, limit, offset)=>{
+    const input = [limit, offset];
+    let query = !cateId ? `
+SELECT 
+	p.id, 
+	p.name AS title, 
+	p.price, 
+	thumbnail AS img, 
+	sc.name AS category,
+    COUNT(r.product_id)
+FROM products p 
+JOIN sub_categories sc ON p.sub_category_id = sc.id
+LEFT JOIN reviews r ON r.product_id = p.id 
+WHERE sub_category_id = ?
+GROUP BY p.id 
+ORDER BY COUNT(r.product_id) DESC
+LIMIT ? OFFSET ? ;
+        ` : `
+    SELECT 
+        p.id, 
+        p.name AS title, 
+        p.price, 
+        thumbnail AS img, 
+        sc.name AS category,
+        COUNT(r.product_id)
+    FROM products p 
+    JOIN sub_categories sc ON p.sub_category_id = sc.id
+    LEFT JOIN reviews r ON r.product_id = p.id 
+    WHERE sub_category_id = ?
+    GROUP BY p.id 
+    ORDER BY COUNT(r.product_id) DESC
+    LIMIT ? OFFSET ? ;` ;
+    
+    if(cateId) input.unshift(cateId);
+        
+    return appDataSource.query(query, input);
+    }
 
 const getTagsByProduct = async (productId) =>{
     return appDataSource.query(`
@@ -73,6 +104,7 @@ const getTagsByProduct = async (productId) =>{
 module.exports = {
     getCategoryId,
     getProductList,
+    getProductListByReview,
     getTagsByProduct,
     getSELLCount
 }
--- api/services/productService.js
+++ api/services/productService.js
@@ -3,31 +3,36 @@ const productDao = require('../models/productDao');
 const getProductList = async (categoryName, limit, offset, sort) => {
     try{
         const cateId =(categoryName !== "all")? await productDao.getCategoryId(categoryName) : 0;
-        console.log(cateId);
         let order;
         limit = Number(limit);
         offset = Number(offset);
+
         const countBySelling = await productDao.getSELLCount();
 
+        let productlist;
         switch(sort){
             case "high" :
                 order = `price DESC`;
+                productlist=await productDao.getProductList( cateId, limit, offset, order);
                 break;
+
             case "low" :
-                order = `price ASC`;
+                order = `price`;
+                productlist=await productDao.getProductList( cateId, limit, offset, order);
                 break;
+
             case "review" :
-                order = `LEFT JOIN reviews r ON r.product_id = p.id
-                WHERE sub_category_id = ?
-                GROUP BY p.id
-                ORDER BY COUNT(r.review)`;
+                order = `COUNT(*)`;
+                productlist=await productDao.getProductListByReview(cateId, limit, offset);
                 break;
-            default :
+                
+                default :
                 order = `AVG(r.score) + COUNT(up.product_id) / (${Number(await countBySelling[0].count)}) * 5 DESC`;
+                productlist=await productDao.getBestProduct(cateId, limit, offset, order);
                 break;
         }
 
-        const productlist = await productDao.getProductList( cateId, limit, offset, order);
+        await productDao.getProductList( cateId, limit, offset, order);
 
         for(let i=0; i<productlist.length; i++){
             const tagArr = [];
