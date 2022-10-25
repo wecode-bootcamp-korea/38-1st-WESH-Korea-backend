923a3c2f24135a43d1bfb2d77f3bba5a07661812
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-10-25 16:08:10 +0900

    수정사항 반영 : 쿼리문 identation 반영 / 프론트 단에서 카테고리 name을 받도록 재설정

--- api/models/productDao.js
+++ api/models/productDao.js
@@ -1,35 +1,15 @@
 const { appDataSource } = require("./appDataSource");
 
-const getCategoryId = async (categoryName) => {
-  const cateId = await appDataSource.query(
-    `
-        SELECT 
-            id
-        FROM sub_categories
-        WHERE name = ?
-    `,
-    [categoryName]
-  );
-  return cateId[0].id;
-};
-
-const getSELLCount = async () => {
-  return appDataSource.query(`
-        SELECT
-            COUNT(*) AS count
-        FROM user_product;
-    `);
-};
-const getProductList = async (cateId, limit, offset, order) => {
+const getProductList = async (categoryId, limit, offset, order) => {
   const input = [limit, offset];
-  let query = !cateId
+  let query = !categoryId
     ? `
 SELECT 
-    p.id, 
-    p.name AS title, 
-    p.price, 
-    thumbnail AS img, 
-    sc.name AS category
+  p.id, 
+  p.name AS title, 
+  p.price, 
+  thumbnail AS img, 
+  sc.name AS category
 FROM products p 
 JOIN sub_categories sc ON p.sub_category_id = sc.id
 LEFT JOIN reviews r ON r.product_id = p.id 
@@ -53,105 +33,102 @@ ORDER BY ${order}
 LIMIT ? OFFSET ?;
 `;
 
-  if (cateId) input.unshift(cateId);
+  if (categoryId) input.unshift(categoryId);
 
   let sol = await appDataSource.query(query, input);
   return sol;
 };
-//sql injection
-const getProductListByReview = async (cateId, limit, offset) => {
-  const input = [limit, offset];
-  let query = !cateId
-    ? `
-SELECT 
-	p.id, 
-	p.name AS title, 
-	p.price, 
-	thumbnail AS img, 
-	sc.name AS category,
-    COUNT(r.product_id)
-FROM products p 
-JOIN sub_categories sc ON p.sub_category_id = sc.id
-LEFT JOIN reviews r ON r.product_id = p.id 
-GROUP BY p.id 
-ORDER BY COUNT(r.product_id) DESC
-LIMIT ? OFFSET ? ;
-        `
-    : `
-    SELECT 
-        p.id, 
-        p.name AS title, 
-        p.price, 
-        thumbnail AS img, 
-        sc.name AS category,
-        COUNT(r.product_id)
-    FROM products p 
-    JOIN sub_categories sc ON p.sub_category_id = sc.id
-    LEFT JOIN reviews r ON r.product_id = p.id 
-    WHERE sub_category_id = ?
-    GROUP BY p.id 
-    ORDER BY COUNT(r.product_id) DESC
-    LIMIT ? OFFSET ? ;`;
 
-  if (cateId) input.unshift(cateId);
+const getProductListByReview = async (categoryId, limit, offset) => {
+  const input = [limit, offset];
+  let query = !categoryId? `
+  SELECT 
+  	p.id, 
+  	p.name AS title, 
+  	p.price, 
+  	thumbnail AS img, 
+  	sc.name AS category,
+    COUNT(r.product_id)
+  FROM products p 
+  JOIN sub_categories sc ON p.sub_category_id = sc.id
+  LEFT JOIN reviews r ON r.product_id = p.id 
+  GROUP BY p.id 
+  ORDER BY COUNT(r.product_id) DESC
+  LIMIT ? OFFSET ? ;
+          `
+    : `
+  SELECT 
+    p.id, 
+    p.name AS title, 
+    p.price, 
+    thumbnail AS img, 
+    sc.name AS category,
+    COUNT(r.product_id)
+  FROM products p 
+  JOIN sub_categories sc ON p.sub_category_id = sc.id
+  LEFT JOIN reviews r ON r.product_id = p.id 
+  WHERE sub_category_id = ?
+  GROUP BY p.id 
+  ORDER BY COUNT(r.product_id) DESC
+  LIMIT ? OFFSET ? ;`;
+
+  if (categoryId) input.unshift(categoryId);
 
   return appDataSource.query(query, input);
 };
 
-const getProductListByHowManySelled = async (cateId, limit, offset) => {
+const getProductListByHowManySelled = async (categoryId, limit, offset) => {
   const input = [limit, offset];
-  let query = !cateId
+  let query = !categoryId
     ? `
-    SELECT 
+  SELECT 
     p.id,
     p.name AS title, 
     p.price, 
     thumbnail AS img, 
     sc.name AS category,
     COUNT(up.product_id)
-    FROM products p 
-    JOIN sub_categories sc ON p.sub_category_id = sc.id
-    LEFT JOIN user_product up ON up.product_id = p.id 
-    GROUP BY p.id 
-    ORDER BY COUNT(up.product_id) DESC
-    LIMIT ? OFFSET ? ;`
-    : `
-    SELECT 
-        p.id, 
-        p.name AS title, 
-        p.price, 
-        thumbnail AS img, 
-        sc.name AS category,
+  FROM products p 
+  JOIN sub_categories sc ON p.sub_category_id = sc.id
+  LEFT JOIN user_product up ON up.product_id = p.id 
+  GROUP BY p.id 
+  ORDER BY COUNT(up.product_id) DESC
+  LIMIT ? OFFSET ? ;`
+  : `
+  SELECT 
+    p.id, 
+    p.name AS title, 
+    p.price, 
+    thumbnail AS img, 
+    sc.name AS category,
     COUNT(up.product_id)
-    FROM products p 
-    JOIN sub_categories sc ON p.sub_category_id = sc.id
-    LEFT JOIN user_product up ON up.product_id = p.id 
-    WHERE sub_category_id = ?
-    GROUP BY p.id 
-    ORDER BY COUNT(up.product_id) DESC
-    LIMIT ? OFFSET ? ;`;
+  FROM products p 
+  JOIN sub_categories sc ON p.sub_category_id = sc.id
+  LEFT JOIN user_product up ON up.product_id = p.id 
+  WHERE sub_category_id = ?
+  GROUP BY p.id 
+  ORDER BY COUNT(up.product_id) DESC
+  LIMIT ? OFFSET ? ;`;
 
-  if (cateId) input.unshift(cateId);
+  if (categoryId) input.unshift(categoryId);
 
   return appDataSource.query(query, input);
 };
 
 const getTagsByProduct = async (productId) => {
-  return appDataSource.query(
-    `
-        SELECT
-            t.name
-        FROM products p
-        LEFT JOIN product_tag pt ON p.id=pt.product_id
-        LEFT JOIN tags t ON t.id=pt.tag_id
-        WHERE p.id = ?
-    `,
-    [productId]
+  return appDataSource.query(`
+    SELECT
+        t.name
+    FROM products p
+    LEFT JOIN product_tag pt ON p.id=pt.product_id
+    LEFT JOIN tags t ON t.id=pt.tag_id
+    WHERE p.id = ?
+    `, [productId]
   );
 };
 
 module.exports = {
-  getCategoryId,
+
   getProductList,
   getProductListByReview,
   getProductListByHowManySelled,
