4e59ceb3ae55faf67432b302d06f41f8efc4598f
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-10-26 01:18:36 +0900

    멘토님... 조금 복잡하긴 하지만, 다오단에서 하나의 함수로 사용할 수 있게끔 구현했습니다. pull 받는 과정에서 stash apply까지 해서 다른 브랜치 파일이 넘어왔지만,, 나름대로 금방 멘탈 회복해서 다시 구현했습니다!!!(그나마) 리팩토링도 하고 코드가 짧아져서 나름 뿌듯합니다. Dao 내의 함수의 인자가 많아진 게 조금 아찔하긴 합니다.

--- api/models/productDao.js
+++ api/models/productDao.js
@@ -1,33 +1,22 @@
 const { appDataSource } = require("./appDataSource");
 
 const getCategoryId = async (categoryName) => {
-  const categoryId = await appDataSource.query(`   
+  const categoryId = await appDataSource.query(`
     SELECT 
-      id
+        id
     FROM sub_categories
     WHERE name = ?
-    `,
-    [categoryName]
-  );
+  `, [categoryName]);
   return categoryId[0].id;
 };
 
-const getProductList = async (categoryId, limit, offset, order) => {
-  const input = [limit, offset];
-  let query = !categoryId ? `
-    SELECT 
-      p.id, 
-      p.name AS title, 
-      p.price, 
-      thumbnail AS img, 
-      sc.name AS category
-    FROM products p 
-    JOIN sub_categories sc ON p.sub_category_id = sc.id
-    LEFT JOIN reviews r ON r.product_id = p.id 
-    GROUP BY p.id
-    ORDER BY ${order}
-    LIMIT ? OFFSET ? ;
-    ` : `
+const getProductList = async (categoryId, limit, offset, joinExpression, isCategory, group, orderBy, limitOffset) => {
+  
+  const input = [limit,offset];
+
+  if(categoryId) input.unshift(categoryId);
+
+  return await appDataSource.query(`
     SELECT 
     	p.id, 
     	p.name AS title, 
@@ -36,110 +25,27 @@ const getProductList = async (categoryId, limit, offset, order) => {
     	sc.name AS category
     FROM products p 
     JOIN sub_categories sc ON p.sub_category_id = sc.id
-    LEFT JOIN reviews r ON r.product_id = p.id 
-    WHERE sub_category_id = ?
-    GROUP BY p.id 
-    ORDER BY ${order}
-    LIMIT ? OFFSET ?;
-    `;
-
-  if (categoryId) input.unshift(categoryId);
-
-  let sol = await appDataSource.query(query, input);
-  return sol;
-};
-
-const getProductListByReview = async (categoryId, limit, offset) => {
-  const input = [limit, offset];
-  let query = !categoryId ? `
-    SELECT 
-    	p.id, 
-    	p.name AS title, 
-    	p.price, 
-    	thumbnail AS img, 
-    	sc.name AS category,
-      COUNT(r.product_id)
-    FROM products p 
-    JOIN sub_categories sc ON p.sub_category_id = sc.id
-    LEFT JOIN reviews r ON r.product_id = p.id 
-    GROUP BY p.id 
-    ORDER BY COUNT(r.product_id) DESC
-    LIMIT ? OFFSET ? ;
-        `
-    : `
-    SELECT 
-      p.id, 
-      p.name AS title, 
-      p.price, 
-      thumbnail AS img, 
-      sc.name AS category,
-      COUNT(r.product_id)
-    FROM products p 
-    JOIN sub_categories sc ON p.sub_category_id = sc.id
-    LEFT JOIN reviews r ON r.product_id = p.id 
-    WHERE sub_category_id = ?
-    GROUP BY p.id 
-    ORDER BY COUNT(r.product_id) DESC
-    LIMIT ? OFFSET ? ;`;
-
-  if (categoryId) input.unshift(categoryId);
-
-  return appDataSource.query(query, input);
-};
-
-const getProductListByHowManySelled = async (categoryId, limit, offset) => {
-  const input = [limit, offset];
-  let query = !categoryId ? `
-    SELECT 
-      p.id,
-      p.name AS title, 
-      p.price, 
-      thumbnail AS img, 
-      sc.name AS category,
-      COUNT(up.product_id)
-      FROM products p 
-    JOIN sub_categories sc ON p.sub_category_id = sc.id
-    LEFT JOIN user_product up ON up.product_id = p.id 
-    GROUP BY p.id 
-    ORDER BY COUNT(up.product_id) DESC
-    LIMIT ? OFFSET ? ;`
-    : `
-    SELECT 
-      p.id, 
-      p.name AS title, 
-      p.price, 
-      thumbnail AS img, 
-      sc.name AS category,
-      COUNT(up.product_id)
-    FROM products p 
-    JOIN sub_categories sc ON p.sub_category_id = sc.id
-    LEFT JOIN user_product up ON up.product_id = p.id 
-    WHERE sub_category_id = ?
-    GROUP BY p.id 
-    ORDER BY COUNT(up.product_id) DESC
-    LIMIT ? OFFSET ? ;`;
-
-  if (categoryId) input.unshift(categoryId);
-
-  return appDataSource.query(query, input);
-};
+    ${joinExpression}
+    ${isCategory}
+    ${group}
+    ${orderBy}
+    ${limitOffset};
+  `, input)
+}
 
 const getTagsByProduct = async (productId) => {
   return appDataSource.query(`
     SELECT
-      t.name
+        t.name
     FROM products p
     LEFT JOIN product_tag pt ON p.id=pt.product_id
     LEFT JOIN tags t ON t.id=pt.tag_id
     WHERE p.id = ?
-    `, [productId]
-  );
+  `, [productId]);
 };
 
 module.exports = {
   getCategoryId,
   getProductList,
-  getProductListByReview,
-  getProductListByHowManySelled,
-  getTagsByProduct,
+  getTagsByProduct
 };
--- api/services/productService.js
+++ api/services/productService.js
@@ -1,53 +1,61 @@
 const productDao = require('../models/productDao');
 
 const getProductList = async (categoryName, limit, offset, sort) => {
+    const categoryId =(categoryName !== "all")? await productDao.getCategoryId(categoryName) : 0;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
     try{
-        const cateId =(categoryName !== "all")? await productDao.getCategoryId(categoryName) : 0;
-        let order;
-        limit = Number(limit);
-        offset = Number(offset);
+    let joinExpression="", isCategory="", group="", orderBy = "";
+    isCategory =  categoryId ? "WHERE sub_category_id = ?" : "";
+    const limitOffset = "LIMIT ? OFFSET ? ;"
+    switch(sort){
+        case "high" :
+            orderBy = "ORDER BY price DESC"
+            break;
+            
+        case "low" :
+            orderBy = "ORDER BY price"
+            break;
 
-        let productlist;
-        switch(sort){
-            case "high" :
-                order = `price DESC`;
-                productlist=await productDao.getProductList( cateId, limit, offset, order);
-                break;
-
-            case "low" :
-                order = `price`;
-                productlist=await productDao.getProductList( cateId, limit, offset, order);
-                break;
-
-            case "review" :
-                productlist=await productDao.getProductListByReview(cateId, limit, offset);
-                break;
+        case "review" :
+            joinExpression = "LEFT JOIN reviews r ON r.product_id = p.id";  
+            group = "GROUP BY p.id";
+            orderBy = "ORDER BY COUNT(r.product_id) DESC"
+            break;
                 
-            default :
-                productlist=await productDao.getProductListByHowManySelled(cateId, limit, offset);
-                break;
+        default :
+            joinExpression = "LEFT JOIN user_product up ON up.product_id = p.id";
+            group = "GROUP BY p.id";
+            orderBy = "ORDER BY COUNT(up.product_id) DESC"
+            break;
+    }
+    const result = await productDao.getProductList(categoryId, limit, offset, joinExpression, isCategory, group, orderBy, limitOffset);
+
+    for(let i=0; i<result.length; i++){
+        const tagArr = [];
+        const productid= result[i].id;
+        const tags = await productDao.getTagsByProduct(productid);
+    
+        for(let j=0; j<tags.length; j++) {
+            tagArr.push(tags[j].name);
         }
-
-
-        for(let i=0; i<productlist.length; i++){
-            const tagArr = [];
-            const productid= await productlist[i].id;
-            const tags = await productDao.getTagsByProduct(productid);
-
-            for(let j=0; j<tags.length; j++) {
-                tagArr.push(tags[j].name);
-            }
-
-            productlist[i].tag = tagArr;
-        }
-
-    return productlist;
-
+    
+        result[i].tag = tagArr;
+    }
+    
+    return result;
+    
     }
     catch(err){
-        throw err;
+    throw err;
     }
-};
+    };
+
+
+
+
 
 module.exports = {
     getProductList
