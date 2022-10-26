f9831e5e73cae065610f48b8f02d315d466a7df8
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-10-26 15:14:33 +0900

    에러핸들링 적용

--- api/controllers/likeController.js
+++ api/controllers/likeController.js
@@ -1,10 +1,10 @@
 const likeService = require("../services/likeService");
+const { catchAsync } = require('../utils/error');
 
-const addLike = async (req, res) => {
+const addLike = catchAsync(async (req, res) => {
   const { user } = req.user;
   const { productId } = req.body;
 
-  try {
     if (!user || !productId) {
       const error = new Error("UNDEFINED_REQUIRED_INPUT");
       error.statusCode=404;
@@ -13,16 +13,13 @@ const addLike = async (req, res) => {
 
     const data = await likeService.addLike(user, productId);
     res.status(201).json({ message: "FUNCTION_SUCCESS", data: data });
-  } catch (error) {
-    res.status(error.statusCode || 500).json(error.message);
-  }
-};
 
-const deleteLike = async (req, res) => {
+});
+
+const deleteLike = catchAsync( async (req, res) => {
   const { user } = req.user;
   const { productId } = req.body;
 
-  try {
     if(!user || !productId) {
         const error = new Error("NOT_FOUND_INPUT");
         error.statusCode = 400;
@@ -31,20 +28,14 @@ const deleteLike = async (req, res) => {
     const data = await likeService.deleteLike(user, productId);
 
     res.status(201).json({ message: "LIKE_DELETED", data: data });
-} catch (error) {
-    res.status(error.statusCode||500).json(error.message);
-}
-};
 
-const getUserLikes = async (req, res) => {
+});
+
+const getUserLikes = catchAsync(async (req, res) => {
     const { user } = req.user;
-    console.log(user);
-  try {
     const data = await likeService.getUserLikes(user);
+
     res.status(201).json({ message: "LOADING_SUCCESS", data: data });
-  } catch (error) {
-    res.status(error.statusCode || 500).json(error.message);
-  }
-};
+});
 
 module.exports = { addLike, deleteLike, getUserLikes };
--- api/routes/likeRouter.js
+++ api/routes/likeRouter.js
@@ -2,11 +2,10 @@ const express = require('express');
 const likeController = require('../controllers/likeController');
 const authentication = require('../utils/auth');
 
-loginRequired
 const router = express.Router();
 
-router.post('/click',  likeController.addLike);
-router.post('/delete',  likeController.deleteLike);
+router.post('/click', authentication.loginRequired, likeController.addLike);
+router.post('/delete', authentication.loginRequired, likeController.deleteLike);
 router.post('/readall', authentication.loginRequired, likeController.getUserLikes);
 
 
