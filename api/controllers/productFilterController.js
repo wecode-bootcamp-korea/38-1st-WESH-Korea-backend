fc69746d1ff3420b5acdbe9ef12e77f457f69c29
김성훈 <m1_air@M1air.local>
2022-10-26 17:00:29 +0900

    에러핸들링 적용

--- api/controllers/productFilterController.js
+++ api/controllers/productFilterController.js
@@ -1,17 +1,14 @@
 const productFilterService = require('../services/productFilterService');
+const { catchAsync } = require('../utils/error');
 
-const priceFilter = async (req, res) => {
+const priceFilter = catchAsync (async (req, res) => {
     const {limit, offset} = req.query;
     const {tag, sort} = req.body;
-    try{
-        const result = await productFilterService.priceFilter(tag, sort, limit, offset);
-        res.status(200).json({data: result});
-    }
-    catch(err){
-        console.log(err);
-        res.status(err.statusCode || 500).json({message: err.message});
-    }
-}
+  
+    const result = await productFilterService.priceFilter(tag, sort, limit, offset);
+    res.status(200).json({data: result});
+   
+})
 
 module.exports = {
     priceFilter
