d25e59aa21240e87d9d11099d0b47ea6e540663d
김성훈 <m1_air@M1air.local>
2022-10-26 16:53:06 +0900

    에러핸들링 적용

--- api/controllers/eventCommentController.js
+++ api/controllers/eventCommentController.js
@@ -1,66 +1,46 @@
 const eventCommentService = require('../services/eventCommentService');
 const jwt = require('jsonwebtoken');
+const { catchAsync } = require('../utils/error');
 
-const getEvReview = async (req, res) => {
+const getEvReview = catchAsync (async (req, res) => {
 
     const evReviewId = +req.params.evTitleId;
+    const evReview = await eventCommentService.getEvReview(evReviewId);
+    res.status(200).json({"data": evReview});
+    
+})
 
-    try{
-        const evReview = await eventCommentService.getEvReview(evReviewId);
-        res.status(200).json({"data": evReview});
-    }
-    catch(err){
-        console.log(err);
-        return res.status(err.statusCode || 500).json({message: err.message});
-    }
-}
-
-const createEvReview = async (req, res) => {
+const createEvReview = catchAsync (async (req, res) => {
 
     const userId = req.user.id;
     const evReviewId = +req.params.evTitleId;
     const { content } = req.body;
 
-    try{
-        await eventCommentService.createEvReview(userId, content, evReviewId);
-        res.status(201).json({"message": "EVENT_REVIEW_CREATED"}); 
-    }
-    catch(err){
-        console.log(err);
-        return res.status(err.statusCode || 500).json({message: err.message});
-    }
-}
+    await eventCommentService.createEvReview(userId, content, evReviewId);
+    res.status(201).json({"message": "EVENT_REVIEW_CREATED"}); 
+   
+})
 
-const modifyEvReview = async (req, res) => {
+const modifyEvReview = catchAsync (async (req, res) => {
 
     const userId = req.user.id;
     const evReviewId = +req.params.evTitleId;
     const { content } = req.body;
 
-    try{
-        await eventCommentService.modifyEvReview(userId, content, evReviewId);
-        res.status(200).json({"message": "EVENT_REVIEW_MODIFIED"}); 
-    }
-    catch(err){
-        console.log(err);
-        return res.status(err.statusCode || 500).json({message: err.message});
-    }
-}
+    await eventCommentService.modifyEvReview(userId, content, evReviewId);
+    res.status(200).json({"message": "EVENT_REVIEW_MODIFIED"}); 
+   
+})
 
-const deleteEvReview = async (req, res) => {
+const deleteEvReview = catchAsync (async (req, res) => {
 
     const userId = req.user.id;
     const evReviewId = +req.params.evTitleId; 
 
-    try{
-        await eventCommentService.deleteEvReview(userId, evReviewId);
-        res.status(200).json({"message": "EVENT_REVIEW_DELETED"}); 
-    }
-    catch(err){
-        console.log(err);
-        return res.status(err.statusCode || 500).json({message: err.message});
-    }
-}
+    await eventCommentService.deleteEvReview(userId, evReviewId);
+    res.status(200).json({"message": "EVENT_REVIEW_DELETED"}); 
+    
+})
 
 module.exports = {
     getEvReview,
